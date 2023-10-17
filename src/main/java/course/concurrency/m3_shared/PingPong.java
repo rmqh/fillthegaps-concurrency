d73027753dd44393227a3439a7b9296189ff069f
rmqh <haqmar@gmail.com>
2023-10-17 20:08:44 +0300

    Add 3-2 ping-pong solution

--- src/main/java/course/concurrency/m3_shared/PingPong.java
+++ src/main/java/course/concurrency/m3_shared/PingPong.java
@@ -2,12 +2,57 @@ package course.concurrency.m3_shared;
 
 public class PingPong {
 
-    public static void ping() {
+    private static final Object OBJ_SYNC = new Object();
+    private static final String PING = "Ping";
+    private static final String PONG = "Pong";
 
+    private static boolean flag;
+
+    public static void ping() {
+        while (true) {
+            synchronized (OBJ_SYNC) {
+                while (flag) {
+                    waitForNotification();
+                }
+                print(PING);
+                flag = !flag;
+                sendNotification();
+            }
+        }
     }
 
     public static void pong() {
+        while (true) {
+            synchronized (OBJ_SYNC) {
+                while (!flag) {
+                    waitForNotification();
+                }
+                print(PONG);
+                flag = !flag;
+                sendNotification();
+            }
+        }
+    }
 
+    private static void waitForNotification() {
+        try {
+            OBJ_SYNC.wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void sendNotification() {
+        OBJ_SYNC.notify();
+    }
+
+    private static void print(String msg) {
+        System.out.println(msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
