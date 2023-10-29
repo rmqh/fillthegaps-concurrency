81b8b6ad820c469f9ea80aef4225c918da55a615
rmqh <haqmar@gmail.com>
2023-10-29 14:02:11 +0300

    Add 3-7 Update AuctionPessimistic

--- src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionPessimistic.java
@@ -3,23 +3,28 @@ package course.concurrency.exams.auction;
 public class AuctionPessimistic implements Auction {
 
     private Notifier notifier;
-    private Bid latestBid;
+    private volatile Bid latestBid;
 
     public AuctionPessimistic(Notifier notifier) {
         this.notifier = notifier;
         this.latestBid = new Bid(0L, 0L, 0L);
     }
 
-    public synchronized boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
+    public boolean propose(Bid bid) {
+        if (latestBid.getPrice() >= bid.getPrice()) {
+            return false;
+        }
+        synchronized (this) {
+            if (latestBid.getPrice() >= bid.getPrice()) {
+                return false;
+            }
             latestBid = bid;
+            notifier.sendOutdatedMessage(latestBid);
             return true;
         }
-        return false;
     }
 
-    public synchronized Bid getLatestBid() {
+    public Bid getLatestBid() {
         return latestBid;
     }
 }
