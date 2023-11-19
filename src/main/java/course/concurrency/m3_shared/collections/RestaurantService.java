e078f5aa67aa4f7807532b9029c5c48f1a5f8494
rmqh <haqmar@gmail.com>
2023-11-19 16:39:10 +0300

    Update RestaurantService

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -3,19 +3,20 @@ package course.concurrency.m3_shared.collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.nonNull;
 
 public class RestaurantService {
 
-    private Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
+    private final Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
         put("A", new Restaurant("A"));
         put("B", new Restaurant("B"));
         put("C", new Restaurant("C"));
     }};
 
-    private final Map<String, Integer> stat = new ConcurrentHashMap<>();
+    private final Map<String, LongAdder> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -23,12 +24,21 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        stat.compute(restaurantName, (name, count) -> nonNull(count) ? ++count : 1);
+        stat.compute(restaurantName, (name, adder) -> {
+            if (nonNull(adder)) {
+                adder.increment();
+                return adder;
+            } else {
+                LongAdder longAdder = new LongAdder();
+                longAdder.increment();
+                return longAdder;
+            }
+        });
     }
 
     public Set<String> printStat() {
         return stat.entrySet().stream()
-            .map(e -> String.format("%s - %s", e.getKey(), e.getValue()))
+            .map(e -> String.format("%s - %s", e.getKey(), e.getValue().longValue()))
             .collect(Collectors.toSet());
     }
 }
